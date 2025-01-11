15cdaeaab9358a1a4cceee0ff83fd3f5340d2c62
Sudipta <sudiptajena89@gmail.com>
2025-01-11 23:12:35 +0530

    Javacodespractice

---
+++ src/ex_01_Java_Basics/Lab004_Comments.java
@@ -0,0 +1,11 @@
+package ex_01_Java_Basics;
+
+public class Lab004_Comments {
+    public static void main(String[] args) {
+//This is a comment, It will be ignored by JVM
+
+        System.out.println("Hi");
+    }
+
+
+}
---
+++ src/ex_01_Java_Basics/Lab008_Comments.java
@@ -0,0 +1,19 @@
+package ex_01_Java_Basics;
+
+public class Lab008_Comments {
+    /**
+     * Author-Sudipta
+     * Course-ATB10x
+     * This is a documentation comment
+     * @param args command-line arguments
+     * @return void
+     *
+     */
+    public static void main(String[] args) {
+        //Method implementation
+
+
+        System.out.println("Checking Comments");
+
+    }
+}
