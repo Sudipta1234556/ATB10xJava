d2d1d5260db6cbab31f7d64ba4f30cad5fa98bae
Sudipta <sudiptajena89@gmail.com>
2025-01-11 23:10:21 +0530

    ShortcutHW

---
+++ src/ex_01_Java_Basics/Lab0010_Common_Math.java
@@ -0,0 +1,11 @@
+package ex_01_Java_Basics;
+
+public class Lab0010_Common_Math {
+    public static void main(String[] args) {
+        System.out.println(2+2);
+        System.out.println(2-2);
+        System.out.println(2/2);
+        System.out.println(2*2);
+        //println-Integer part-No Decimal
+    }
+}
---
+++ src/ex_01_Java_Basics/Lab0012_Println_vsPrint.java
@@ -0,0 +1,10 @@
+package ex_01_Java_Basics;
+
+public class Lab0012_Println_vsPrint {
+    public static void main(String[] args) {
+//        System.out.println("Hello World1");
+//        System.out.println("Hello World2");
+        System.out.print("Hello World3");
+        System.out.print("Hello World4");
+    }
+}
