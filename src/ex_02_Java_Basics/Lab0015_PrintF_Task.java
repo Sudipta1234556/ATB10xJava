075d6f54e61b9e36a9befd564c57734596c254c3
Sudipta <sudiptajena89@gmail.com>
2025-01-12 12:28:47 +0530

    Printf

---
+++ src/ex_02_Java_Basics/Lab0015_PrintF_Task.java
@@ -0,0 +1,17 @@
+package ex_02_Java_Basics;
+
+public class Lab0015_PrintF_Task {
+    public static void main(String[] args) {
+        int a = 9;
+        System.out.printf(" %d\n",a*1);
+        System.out.printf(" %d\n",a*2);
+        System.out.printf(" %d\n",a*3);
+        System.out.printf(" %d\n",a*4);
+        System.out.printf(" %d\n",a*5);
+        System.out.printf(" %d\n",a*6);
+        System.out.printf(" %d\n",a*7);
+        System.out.printf(" %d\n",a*8);
+        System.out.printf(" %d\n",a*9);
+        System.out.printf(" %d\n",a*10);
+    }
+}
