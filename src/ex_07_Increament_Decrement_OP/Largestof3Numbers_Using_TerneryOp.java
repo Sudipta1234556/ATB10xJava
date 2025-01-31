1df314d330c8e89ed543abdf18872940017dbbb6
Sudipta <sudiptajena89@gmail.com>
2025-01-31 21:21:46 +0530

    Largestof3

--- src/ex_07_Increament_Decrement_OP/Largestof3Numbers_Using_TerneryOp.java
+++ src/ex_07_Increament_Decrement_OP/Largestof3Numbers_Using_TerneryOp.java
@@ -2,7 +2,7 @@ package ex_07_Increament_Decrement_OP;
 
 public class Largestof3Numbers_Using_TerneryOp {
     public static void main(String[] args) {
-        int n1 = 50, n2 = 70, n3 = 40;
+        int n1 = 60, n2 = 70, n3 = 50;
 
         int largest = (n1 >= n2) ? ((n1 >= n3) ? n1 : n3) : ((n2>=n3) ? n2 : n3);
         System.out.println(largest);
